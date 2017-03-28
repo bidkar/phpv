a72a2866b7fa9bdad2e7367e27ea5a63c7e19255
Bidkar Aragon Cardenas <bidkar@cetis108.edu.mx>
2017-03-28 08:11:37 -0700

    correccion index

--- index.php
+++ index.php
@@ -90,7 +90,6 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="public/js/jquery-3.1.1.slim.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
     <script src="public/js/bootstrap.min.js"></script>
   </body>
 </html>
