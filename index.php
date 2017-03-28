bbffad3820122678e5bb60019e7b5815b3752711
Bidkar Aragon Cardenas <bidkar@cetis108.edu.mx>
2017-03-28 07:53:03 -0700

    bootstrap + jquery
    - se organizaron las carpetas para modelos, vistas, public
    - se agrego landing page

--- index.php
+++ index.php
@@ -1,60 +1,96 @@
-<?php
-require 'conexion.php';
-require 'usuario.php';
-
-// validar sesion iniciado
-session_start();
-if (!isset($_SESSION['usuario'])) {
-    header('Location:views/login.php');
-}
-// fin validacion
-
-?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Apache + PHP funciona!</title>
-</head>
-<body>
-    <h1><?php echo 'Apache + PHP funciona!'; ?></h1>
-    <?php
-    $usuario = new Usuario();
-    $usuario->id = 10;
-    $usuario->nombres = 'Bidkar';
-    ?>
-    <table>
-        <thead>
-            <tr>
-                <th>UserID</th>
-                <th>Nombre</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td><?php echo $usuario->id; ?></td>
-                <td><?php echo $usuario->nombres; ?></td>
-            </tr>
-        </tbody>
-    </table>
-    <div style="padding:50px;background-color:gray;">
-        <form action="#">
-            <label for="userid">ID de Usuario</label>
-            <input type="text" name="userid" id="userid">
-            <button type="submit">Buscar por ID</button>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Jumbotron Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="public/css/bootstrap.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="#">Navbar</a>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
+      </div>
+    </nav>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1 class="display-3">Hello, world!</h1>
+        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+      </div>
     </div>
-    <?php
-    // leer la variable via metodo get
-    // isset() evalua que exista y que sea distinta de null
-    if (isset($_GET['userid'])) {
-        $id = $_GET['userid'];
-        if ($usuario->buscarPorId($id)) {
-            var_dump($usuario);
-        } else {
-            echo 'usuario no encontrado';
-        }
-    }
-    ?>
-</body>
+
+    <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-md-4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+       </div>
+        <div class="col-md-4">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+      </div>
+
+      <hr>
+
+      <footer>
+        <p>&copy; Company 2017</p>
+      </footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="public/js/jquery-3.1.1.slim.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="public/js/bootstrap.min.js"></script>
+  </body>
 </html>
---
+++ views/home.php
@@ -0,0 +1,60 @@
+<?php
+require 'conexion.php';
+require 'usuario.php';
+
+// validar sesion iniciado
+session_start();
+if (!isset($_SESSION['usuario'])) {
+    header('Location:views/login.php');
+}
+// fin validacion
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Apache + PHP funciona!</title>
+</head>
+<body>
+    <h1><?php echo 'Apache + PHP funciona!'; ?></h1>
+    <?php
+    $usuario = new Usuario();
+    $usuario->id = 10;
+    $usuario->nombres = 'Bidkar';
+    ?>
+    <table>
+        <thead>
+            <tr>
+                <th>UserID</th>
+                <th>Nombre</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><?php echo $usuario->id; ?></td>
+                <td><?php echo $usuario->nombres; ?></td>
+            </tr>
+        </tbody>
+    </table>
+    <div style="padding:50px;background-color:gray;">
+        <form action="#">
+            <label for="userid">ID de Usuario</label>
+            <input type="text" name="userid" id="userid">
+            <button type="submit">Buscar por ID</button>
+        </form>
+    </div>
+    <?php
+    // leer la variable via metodo get
+    // isset() evalua que exista y que sea distinta de null
+    if (isset($_GET['userid'])) {
+        $id = $_GET['userid'];
+        if ($usuario->buscarPorId($id)) {
+            var_dump($usuario);
+        } else {
+            echo 'usuario no encontrado';
+        }
+    }
+    ?>
+</body>
+</html>
